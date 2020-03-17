3cfc7192f5d87b46bdc5de35c48190a4a6987fc7
WeiBoHuang <twer40310@gmail.com>
2020-03-17 10:18:28 +0800

    新增model

--- 套件練習/Django/musics/models.py
+++ 套件練習/Django/musics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+class Music(models.Model):
+    song = models.TextField(default="song")
+    singer = models.TextField(default="default_singer")
+    last_modifty_date=models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "music"
